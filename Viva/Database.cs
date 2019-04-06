aeacec0f651187de38e56a71e324e5c45a3548d4
Mhd Fazil <mhdfazil79@gmail.com>
2019-04-06 21:59:55 +0530

    databse class updated

--- Viva/Database.cs
+++ Viva/Database.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data;
 
 namespace Viva
 {
@@ -14,8 +16,48 @@ namespace Viva
         public Database()
         {
             con = new SqlConnection();
-            con.ConnectionString= "DESKTOP-3HUH0OS (DESKTOP-3HUH0OS\\Mhd Fazil)";
+            con.ConnectionString = "Data Source=DESKTOP-3HUH0OS;Initial Catalog=Garment;Integrated Security=True";
         }
-
+        public void openCon()
+        {
+            con.Open();
+        }
+        public void closeCon()
+        {
+            con.Close();
+        }
+        public int save_delete_update(string query)
+        {
+            int row;
+            try
+            {
+                openCon();
+            }
+            catch
+            {
+                MessageBox.Show("Check your database connection");
+            }
+            cmd = new SqlCommand(query, con);
+            row = cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            closeCon();
+            return row;
+        }
+        public DataTable GetData(string query)
+        {
+            try
+            {
+                openCon();
+            }
+            catch
+            {
+                MessageBox.Show("Check your database connection");
+            }
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            closeCon();
+            return dt;
+        }
     }
 }
