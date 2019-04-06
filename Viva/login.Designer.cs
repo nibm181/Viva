1b7c63d0a65cb2275f2d5753e6cd45f40d8c41eb
Mhd Fazil <mhdfazil79@gmail.com>
2019-04-06 18:01:20 +0530

    changes text position

--- Viva/login.Designer.cs
+++ Viva/login.Designer.cs
@@ -38,9 +38,10 @@
             // 
             this.metroLabel1.AutoSize = true;
             this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.metroLabel1.Location = new System.Drawing.Point(873, 398);
+            this.metroLabel1.Location = new System.Drawing.Point(631, 319);
+            this.metroLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.metroLabel1.Name = "metroLabel1";
-            this.metroLabel1.Size = new System.Drawing.Size(100, 25);
+            this.metroLabel1.Size = new System.Drawing.Size(97, 25);
             this.metroLabel1.TabIndex = 0;
             this.metroLabel1.Text = "User Name";
             // 
@@ -48,9 +49,10 @@
             // 
             this.metroLabel2.AutoSize = true;
             this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.metroLabel2.Location = new System.Drawing.Point(888, 468);
+            this.metroLabel2.Location = new System.Drawing.Point(646, 375);
+            this.metroLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.metroLabel2.Name = "metroLabel2";
-            this.metroLabel2.Size = new System.Drawing.Size(85, 25);
+            this.metroLabel2.Size = new System.Drawing.Size(82, 25);
             this.metroLabel2.TabIndex = 0;
             this.metroLabel2.Text = "Password";
             this.metroLabel2.Click += new System.EventHandler(this.metroLabel2_Click);
@@ -61,9 +63,10 @@
             // 
             // 
             this.txt_Uname.CustomButton.Image = null;
-            this.txt_Uname.CustomButton.Location = new System.Drawing.Point(154, 2);
+            this.txt_Uname.CustomButton.Location = new System.Drawing.Point(86, 2);
+            this.txt_Uname.CustomButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_Uname.CustomButton.Name = "";
-            this.txt_Uname.CustomButton.Size = new System.Drawing.Size(27, 27);
+            this.txt_Uname.CustomButton.Size = new System.Drawing.Size(16, 17);
             this.txt_Uname.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
             this.txt_Uname.CustomButton.TabIndex = 1;
             this.txt_Uname.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
@@ -72,7 +75,8 @@
             this.txt_Uname.FontSize = MetroFramework.MetroTextBoxSize.Tall;
             this.txt_Uname.FontWeight = MetroFramework.MetroTextBoxWeight.Light;
             this.txt_Uname.Lines = new string[0];
-            this.txt_Uname.Location = new System.Drawing.Point(991, 391);
+            this.txt_Uname.Location = new System.Drawing.Point(743, 318);
+            this.txt_Uname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_Uname.MaxLength = 32767;
             this.txt_Uname.Multiline = true;
             this.txt_Uname.Name = "txt_Uname";
@@ -82,7 +86,7 @@
             this.txt_Uname.SelectionLength = 0;
             this.txt_Uname.SelectionStart = 0;
             this.txt_Uname.ShortcutsEnabled = true;
-            this.txt_Uname.Size = new System.Drawing.Size(184, 32);
+            this.txt_Uname.Size = new System.Drawing.Size(138, 26);
             this.txt_Uname.TabIndex = 1;
             this.txt_Uname.UseSelectable = true;
             this.txt_Uname.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -94,9 +98,10 @@
             // 
             // 
             this.txt_Pword.CustomButton.Image = null;
-            this.txt_Pword.CustomButton.Location = new System.Drawing.Point(154, 2);
+            this.txt_Pword.CustomButton.Location = new System.Drawing.Point(86, 2);
+            this.txt_Pword.CustomButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_Pword.CustomButton.Name = "";
-            this.txt_Pword.CustomButton.Size = new System.Drawing.Size(27, 27);
+            this.txt_Pword.CustomButton.Size = new System.Drawing.Size(16, 17);
             this.txt_Pword.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
             this.txt_Pword.CustomButton.TabIndex = 1;
             this.txt_Pword.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
@@ -105,7 +110,8 @@
             this.txt_Pword.FontSize = MetroFramework.MetroTextBoxSize.Tall;
             this.txt_Pword.FontWeight = MetroFramework.MetroTextBoxWeight.Light;
             this.txt_Pword.Lines = new string[0];
-            this.txt_Pword.Location = new System.Drawing.Point(991, 461);
+            this.txt_Pword.Location = new System.Drawing.Point(743, 375);
+            this.txt_Pword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_Pword.MaxLength = 32767;
             this.txt_Pword.Multiline = true;
             this.txt_Pword.Name = "txt_Pword";
@@ -115,7 +121,7 @@
             this.txt_Pword.SelectionLength = 0;
             this.txt_Pword.SelectionStart = 0;
             this.txt_Pword.ShortcutsEnabled = true;
-            this.txt_Pword.Size = new System.Drawing.Size(184, 32);
+            this.txt_Pword.Size = new System.Drawing.Size(138, 26);
             this.txt_Pword.TabIndex = 1;
             this.txt_Pword.UseSelectable = true;
             this.txt_Pword.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -123,19 +129,19 @@
             // 
             // form_login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Viva.Properties.Resources.section_3;
             this.BackImage = global::Viva.Properties.Resources.section_3;
             this.BackMaxSize = 1300;
-            this.ClientSize = new System.Drawing.Size(1235, 583);
+            this.ClientSize = new System.Drawing.Size(926, 474);
             this.Controls.Add(this.txt_Pword);
             this.Controls.Add(this.txt_Uname);
             this.Controls.Add(this.metroLabel2);
             this.Controls.Add(this.metroLabel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "form_login";
-            this.Padding = new System.Windows.Forms.Padding(19, 60, 19, 20);
+            this.Padding = new System.Windows.Forms.Padding(14, 49, 14, 16);
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
