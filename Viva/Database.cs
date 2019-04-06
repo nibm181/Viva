780f652fe5703ec394112feb1b1cd147cd4a42c7
Mhd Fazil <mhdfazil79@gmail.com>
2019-04-06 18:34:14 +0530

    added database class

---
+++ Viva/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Viva
+{
+    class Database
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        public Database()
+        {
+            con = new SqlConnection();
+            con.ConnectionString= "DESKTOP-3HUH0OS (DESKTOP-3HUH0OS\\Mhd Fazil)";
+        }
+
+    }
+}
