be41b90cc5a9051141caa694f25f319e26383a3a
doof warrior <siroshang@gmail.com>
2019-04-09 16:18:08 +0530

    material interface

---
+++ Viva/Materials.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Viva
+{
+    partial class Materials
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_gar_id = new MetroFramework.Controls.MetroTextBox();
+            this.Gar_id = new MetroFramework.Controls.MetroLabel();
+            this.txt_gar_length = new MetroFramework.Controls.MetroTextBox();
+            this.txt_gar_type = new MetroFramework.Controls.MetroTextBox();
+            this.txt_gar_name = new MetroFramework.Controls.MetroTextBox();
+            this.gar_type = new MetroFramework.Controls.MetroLabel();
+            this.garI_name = new MetroFramework.Controls.MetroLabel();
+            this.cmb_gar_length = new MetroFramework.Controls.MetroComboBox();
+            this.SuspendLayout();
+            // 
+            // txt_gar_id
+            // 
+            // 
+            // 
+            // 
+            this.txt_gar_id.CustomButton.Image = null;
+            this.txt_gar_id.CustomButton.Location = new System.Drawing.Point(235, 1);
+            this.txt_gar_id.CustomButton.Name = "";
+            this.txt_gar_id.CustomButton.Size = new System.Drawing.Size(33, 33);
+            this.txt_gar_id.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txt_gar_id.CustomButton.TabIndex = 1;
+            this.txt_gar_id.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txt_gar_id.CustomButton.UseSelectable = true;
+            this.txt_gar_id.CustomButton.Visible = false;
+            this.txt_gar_id.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.txt_gar_id.Lines = new string[0];
+            this.txt_gar_id.Location = new System.Drawing.Point(225, 117);
+            this.txt_gar_id.MaxLength = 32767;
+            this.txt_gar_id.Multiline = true;
+            this.txt_gar_id.Name = "txt_gar_id";
+            this.txt_gar_id.PasswordChar = '\0';
+            this.txt_gar_id.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txt_gar_id.SelectedText = "";
+            this.txt_gar_id.SelectionLength = 0;
+            this.txt_gar_id.SelectionStart = 0;
+            this.txt_gar_id.ShortcutsEnabled = true;
+            this.txt_gar_id.Size = new System.Drawing.Size(269, 35);
+            this.txt_gar_id.TabIndex = 0;
+            this.txt_gar_id.UseSelectable = true;
+            this.txt_gar_id.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txt_gar_id.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.txt_gar_id.Click += new System.EventHandler(this.metroTextBox1_Click);
+            // 
+            // Gar_id
+            // 
+            this.Gar_id.AutoSize = true;
+            this.Gar_id.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.Gar_id.Location = new System.Drawing.Point(46, 127);
+            this.Gar_id.Name = "Gar_id";
+            this.Gar_id.Size = new System.Drawing.Size(104, 25);
+            this.Gar_id.TabIndex = 1;
+            this.Gar_id.Text = "Garment ID";
+            this.Gar_id.Click += new System.EventHandler(this.metroLabel1_Click);
+            // 
+            // txt_gar_length
+            // 
+            // 
+            // 
+            // 
+            this.txt_gar_length.CustomButton.Image = null;
+            this.txt_gar_length.CustomButton.Location = new System.Drawing.Point(235, 1);
+            this.txt_gar_length.CustomButton.Name = "";
+            this.txt_gar_length.CustomButton.Size = new System.Drawing.Size(33, 33);
+            this.txt_gar_length.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txt_gar_length.CustomButton.TabIndex = 1;
+            this.txt_gar_length.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txt_gar_length.CustomButton.UseSelectable = true;
+            this.txt_gar_length.CustomButton.Visible = false;
+            this.txt_gar_length.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.txt_gar_length.Lines = new string[0];
+            this.txt_gar_length.Location = new System.Drawing.Point(225, 319);
+            this.txt_gar_length.MaxLength = 32767;
+            this.txt_gar_length.Multiline = true;
+            this.txt_gar_length.Name = "txt_gar_length";
+            this.txt_gar_length.PasswordChar = '\0';
+            this.txt_gar_length.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txt_gar_length.SelectedText = "";
+            this.txt_gar_length.SelectionLength = 0;
+            this.txt_gar_length.SelectionStart = 0;
+            this.txt_gar_length.ShortcutsEnabled = true;
+            this.txt_gar_length.Size = new System.Drawing.Size(269, 35);
+            this.txt_gar_length.TabIndex = 2;
+            this.txt_gar_length.UseSelectable = true;
+            this.txt_gar_length.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txt_gar_length.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // txt_gar_type
+            // 
+            // 
+            // 
+            // 
+            this.txt_gar_type.CustomButton.Image = null;
+            this.txt_gar_type.CustomButton.Location = new System.Drawing.Point(235, 1);
+            this.txt_gar_type.CustomButton.Name = "";
+            this.txt_gar_type.CustomButton.Size = new System.Drawing.Size(33, 33);
+            this.txt_gar_type.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txt_gar_type.CustomButton.TabIndex = 1;
+            this.txt_gar_type.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txt_gar_type.CustomButton.UseSelectable = true;
+            this.txt_gar_type.CustomButton.Visible = false;
+            this.txt_gar_type.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.txt_gar_type.Lines = new string[0];
+            this.txt_gar_type.Location = new System.Drawing.Point(225, 245);
+            this.txt_gar_type.MaxLength = 32767;
+            this.txt_gar_type.Multiline = true;
+            this.txt_gar_type.Name = "txt_gar_type";
+            this.txt_gar_type.PasswordChar = '\0';
+            this.txt_gar_type.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txt_gar_type.SelectedText = "";
+            this.txt_gar_type.SelectionLength = 0;
+            this.txt_gar_type.SelectionStart = 0;
+            this.txt_gar_type.ShortcutsEnabled = true;
+            this.txt_gar_type.Size = new System.Drawing.Size(269, 35);
+            this.txt_gar_type.TabIndex = 3;
+            this.txt_gar_type.UseSelectable = true;
+            this.txt_gar_type.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txt_gar_type.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // txt_gar_name
+            // 
+            // 
+            // 
+            // 
+            this.txt_gar_name.CustomButton.Image = null;
+            this.txt_gar_name.CustomButton.Location = new System.Drawing.Point(235, 1);
+            this.txt_gar_name.CustomButton.Name = "";
+            this.txt_gar_name.CustomButton.Size = new System.Drawing.Size(33, 33);
+            this.txt_gar_name.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txt_gar_name.CustomButton.TabIndex = 1;
+            this.txt_gar_name.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txt_gar_name.CustomButton.UseSelectable = true;
+            this.txt_gar_name.CustomButton.Visible = false;
+            this.txt_gar_name.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.txt_gar_name.Lines = new string[0];
+            this.txt_gar_name.Location = new System.Drawing.Point(225, 180);
+            this.txt_gar_name.MaxLength = 32767;
+            this.txt_gar_name.Multiline = true;
+            this.txt_gar_name.Name = "txt_gar_name";
+            this.txt_gar_name.PasswordChar = '\0';
+            this.txt_gar_name.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txt_gar_name.SelectedText = "";
+            this.txt_gar_name.SelectionLength = 0;
+            this.txt_gar_name.SelectionStart = 0;
+            this.txt_gar_name.ShortcutsEnabled = true;
+            this.txt_gar_name.Size = new System.Drawing.Size(269, 35);
+            this.txt_gar_name.TabIndex = 4;
+            this.txt_gar_name.UseSelectable = true;
+            this.txt_gar_name.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txt_gar_name.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // gar_type
+            // 
+            this.gar_type.AutoSize = true;
+            this.gar_type.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.gar_type.Location = new System.Drawing.Point(43, 255);
+            this.gar_type.Name = "gar_type";
+            this.gar_type.Size = new System.Drawing.Size(50, 25);
+            this.gar_type.TabIndex = 6;
+            this.gar_type.Text = "Type";
+            this.gar_type.Click += new System.EventHandler(this.metroLabel3_Click);
+            // 
+            // garI_name
+            // 
+            this.garI_name.AutoSize = true;
+            this.garI_name.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.garI_name.Location = new System.Drawing.Point(43, 190);
+            this.garI_name.Name = "garI_name";
+            this.garI_name.Size = new System.Drawing.Size(60, 25);
+            this.garI_name.TabIndex = 7;
+            this.garI_name.Text = "Name";
+            this.garI_name.Click += new System.EventHandler(this.metroLabel4_Click);
+            // 
+            // cmb_gar_length
+            // 
+            this.cmb_gar_length.FontSize = MetroFramework.MetroComboBoxSize.Tall;
+            this.cmb_gar_length.FormattingEnabled = true;
+            this.cmb_gar_length.ItemHeight = 29;
+            this.cmb_gar_length.Items.AddRange(new object[] {
+            "Length",
+            "Weight"});
+            this.cmb_gar_length.Location = new System.Drawing.Point(43, 319);
+            this.cmb_gar_length.Name = "cmb_gar_length";
+            this.cmb_gar_length.Size = new System.Drawing.Size(121, 35);
+            this.cmb_gar_length.TabIndex = 8;
+            this.cmb_gar_length.UseSelectable = true;
+            // 
+            // Materials
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 664);
+            this.Controls.Add(this.cmb_gar_length);
+            this.Controls.Add(this.garI_name);
+            this.Controls.Add(this.gar_type);
+            this.Controls.Add(this.txt_gar_name);
+            this.Controls.Add(this.txt_gar_type);
+            this.Controls.Add(this.txt_gar_length);
+            this.Controls.Add(this.Gar_id);
+            this.Controls.Add(this.txt_gar_id);
+            this.Name = "Materials";
+            this.Text = "Materials";
+            this.Load += new System.EventHandler(this.Materials_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox txt_gar_id;
+        private MetroFramework.Controls.MetroLabel Gar_id;
+        private MetroFramework.Controls.MetroTextBox txt_gar_length;
+        private MetroFramework.Controls.MetroTextBox txt_gar_type;
+        private MetroFramework.Controls.MetroTextBox txt_gar_name;
+        private MetroFramework.Controls.MetroLabel gar_type;
+        private MetroFramework.Controls.MetroLabel garI_name;
+        private MetroFramework.Controls.MetroComboBox cmb_gar_length;
+    }
+}
---
+++ Viva/Materials.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Viva
+{
+    public partial class Materials : MetroFramework.Forms.MetroForm
+    {
+        public Materials()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Materials_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
