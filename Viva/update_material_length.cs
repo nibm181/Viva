71fe70db9d577f0a479b0a5cc60b1490742dbe4d
doof warrior <siroshang@gmail.com>
2019-04-12 22:40:27 +0530

    update material length has been added. Make sure it is available..

--- Viva/update_material_length.cs
+++ Viva/update_material_length.cs
@@ -34,27 +34,19 @@ namespace Viva
             {
                 MetroMessageBox.Show(this, "Please Enter Material Name or ID or Type!", "Empty Values", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
-
             else
             {
                 string search = txt_umat_search.Text;
                 Database db = new Database();
                 DataTable d = new DataTable();
                 d = db.GetData("select mat_id, mat_name, mat_type, mat_length from materials where mat_type like '" + search + "' or mat_name like '" + search + "' or mat_id like '" + search + "'");
-                if (d != null)
-                {
-                    MetroMessageBox.Show(this, "No data found!", "No Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                else
-                {
-                    grd_update_mat.DataSource = d;
-                    grd_update_mat.Columns[0].Name = "Material ID";
-                    grd_update_mat.Columns[1].Name = "Name";
-                    grd_update_mat.Columns[2].Name = "Type";
-                    grd_update_mat.Columns[3].Name = "Length";
-                }
+                grd_update_mat.DataSource = d;
+                grd_update_mat.Columns[0].Name = "Material ID";
+                grd_update_mat.Columns[1].Name = "Name";
+                grd_update_mat.Columns[2].Name = "Type";
+                grd_update_mat.Columns[3].Name = "Length";
             }
+            
 
         }
 
@@ -69,15 +61,19 @@ namespace Viva
         {
             if (string.IsNullOrWhiteSpace(txt_umat_id.Text))
             {
-                MetroMessageBox.Show(this, "Please Select Material from Table!", "Empty Values", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MetroMessageBox.Show(this, "Please select material from the table!", "Material not selected", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else if (string.IsNullOrWhiteSpace(txt_umat_ul.Text))
             {
-                MetroMessageBox.Show(this, "Please Enter Used Length!", "Empty Values", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MetroMessageBox.Show(this, "Please Enter Used Material Length!", "Empty Values", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else if (string.IsNullOrWhiteSpace(txt_umat_note.Text))
             {
                 MetroMessageBox.Show(this, "Please Enter Note!", "Empty Values", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txt_umat_ul.Text.Any(c => char.IsLetter(c)))
+            {
+                MetroMessageBox.Show(this, "Please Enter Valid used length value!", "Invalid Values", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
@@ -91,6 +87,7 @@ namespace Viva
                 db.save_delete_update("update materials set mat_length = '" + ans + "' where mat_id ='" + txt_umat_id.Text + "' ");
                 db.save_delete_update("insert into material_used values('" + txt_umat_id.Text + "', '" + now + "', '" + txt_umat_note.Text + "', '" + ulen + "') ");
             }
+            
 
         }
     }
