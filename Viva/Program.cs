4ac942aa404bb792529b6860a5a89f1e26813c2d
Eshan Eshan <iameshan09@gmail.com>
2019-04-18 12:33:11 +0530

    editGarment updated

--- Viva/Program.cs
+++ Viva/Program.cs
@@ -16,7 +16,7 @@ namespace Viva
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new update_material_length());
+            Application.Run(new editGarment());
         }
     }
 }
