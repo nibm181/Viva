53f5f71ade14f0840562eb252ce67db76d96e56f
Mhd Fazil <mhdfazil79@gmail.com>
2019-05-23 23:03:12 +0530

    add bill

---
+++ Viva/BillReportForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Viva
+{
+    public partial class BillReportForm : MetroFramework.Forms.MetroForm
+    {
+        public BillReportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BillReportForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'Bill.DataTable1' table. You can move, or remove it, as needed.
+            this.DataTable1TableAdapter.Fill(this.Bill.DataTable1, "O0001");
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
--- Viva/Program.cs
+++ Viva/Program.cs
@@ -16,7 +16,7 @@ namespace Viva
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Order1());
+            Application.Run(new BillReportForm());
         }
     }
 }
